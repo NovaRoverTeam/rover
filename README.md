26ed40e30bb9e567d4de02db0faf59e9bdc64bdf
Benjamiiiin <bensteer0@gmail.com>
2017-07-06 18:17:44 +1000

    Added pwm servo test.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # rover
 Contains the software packages to run on the Mars rover.
+
+Dependencies:
+
+- WiringPi (http://wiringpi.com/download-and-install/)
+
+- pca9685 (https://github.com/Reinbert/pca9685)
--- src/underling.cpp
+++ src/underling.cpp
@@ -1,67 +1,41 @@
 #include "ros/ros.h"
 #include <ros/console.h>
 #include <math.h>
-//#include "rover/RawControl.h"
-//#include "rover/Command.h"
 
-class PubScrub
-{
-public:
-  PubScrub()
-  {
-    pub_ = n_.advertise<rover::Command>("command_data", 1000);
+#include <wiringPi.h>
+#include <pca9685.h>
 
-    scrub_ = n_.subscribe("control_data", 1000, &PubScrub::control_dataCallback, this);
-  }
-
-  void control_dataCallback(const rover::RawControl::ConstPtr& msg)
-  {
-    //ROS_INFO("Axis %d has value %.5f", msg->id, msg->length);
-
-    rover::Command cmd_msg;
-    cmd_msg.system = 0;
-    cmd_msg.target = 0;
-
-    bool driving = false;
-
-    
-    ROS_INFO_STREAM(msg->axis_x); 
-    ROS_INFO_STREAM(fabs(msg->axis_y)); 
-
-    if (fabs(msg->axis_y) > stick_threshold) 
-    {
-      driving = true;
-      //ROS_INFO("DRIVE");
-      cmd_msg.command = 1; // Drive
-      cmd_msg.value1 = msg->axis_y;
-      cmd_msg.value2 = msg->axis_y;
-    }
-    
-    if ((!driving) && (fabs(msg->axis_x) > stick_threshold))
-    {
-      //ROS_INFO("STEER");
-      cmd_msg.command = 2; // Steer
-      cmd_msg.value1 = -(msg->axis_x);
-      cmd_msg.value2 = msg->axis_x;
-    }
-  
-    pub_.publish(cmd_msg);      
-  }
-
-private:
-  ros::NodeHandle n_; 
-  ros::Publisher pub_;
-  ros::Subscriber scrub_;
-
-}; // END OF CLASS - PubScrub
+#define PIN_BASE 160
+#define MAX_PWM 4096
+#define HERTZ 50
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "underling");
+  ros::Rate loop_rate(50); // 50 Hz
 
-  PubScrub PS; // Create PubScrub object
+  bool increasing = true;
+  int value = 1000;
 
-  ros::spin();
+  pca9685Setup(PIN_BASE, 0x00, HERTZ);
+
+  while (ros::ok())
+  {
+    pwmWrite(PIN_BASE + 4, value);
+  
+    if (increasing) 
+      value += 50;
+    else
+      value -= 50;
+    
+    if (value > 3000)
+      increasing = false;
+    else if (value < 1000)
+      increasing = true;
+
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
 
   return 0;
 }
