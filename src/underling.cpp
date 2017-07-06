7e14f00569de06b0f2448fa7705198d5cfb6070c
benjamiiiin <bensteer0@gmail.com>
2017-07-06 19:20:38 +1000

    Got servo example working.

--- src/underling.cpp
+++ src/underling.cpp
@@ -9,29 +9,48 @@
 #define MAX_PWM 4096
 #define HERTZ 50
 
+
+// Calculate the number of ticks the signal should be high for the required amount of time
+int calcTicks(float impulseMs, int hertz)
+{
+  float cycleMs = 1000.0f / hertz;
+  return (int)(MAX_PWM * impulseMs / cycleMs + 0.5f);
+}
+
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "underling");
+  ros::NodeHandle n;
   ros::Rate loop_rate(50); // 50 Hz
 
   bool increasing = true;
-  int value = 1000;
+  int value = 200;
 
-  pca9685Setup(PIN_BASE, 0x00, HERTZ);
+  int fd = pca9685Setup(PIN_BASE, 0x40, HERTZ);
+  if (fd < 0)
+  {
+    printf("Error in setup\n");
+    return fd;
+  }
+
+  // Reset all output
+  pca9685PWMReset(fd);
 
   while (ros::ok())
   {
     pwmWrite(PIN_BASE + 4, value);
   
     if (increasing) 
-      value += 50;
+      value += 1;
     else
-      value -= 50;
+      value -= 1;
     
-    if (value > 3000)
+    if (value > 400)
       increasing = false;
-    else if (value < 1000)
+    else if (value < 200)
       increasing = true;
+	
+    ROS_INFO_STREAM(value);
 
     ros::spinOnce();
     loop_rate.sleep();
